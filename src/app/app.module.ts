b972e36c2aed37cdfb125c4b46ff569a9ee3b40e
Jan Szataniak <jan.szataniak@gmail.com>
2020-10-18 15:11:01 +0200

    Routing, home page and mobile responsivity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent
   ],
-    imports: [
-        BrowserModule,
-    ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
